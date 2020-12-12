d988711cd605a9d9b836d08b0f57d169e8a1e8af
Kamil Borowski <k.borowski@onet.pl>
2020-12-12 11:55:07 +0100

    create backend server and basic configure

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Zalogowano do bazy danych")
+);
+
+app.use(express.json());
+app.use(cors({ origin: true, credentials: true }));
+
+app.listen(5000, () =>
+  console.log("serwer włączony na porcie 5000: http://localhost:5000")
+);
